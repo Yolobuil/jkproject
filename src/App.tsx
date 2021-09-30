fedc801bb9bffdc4faf2d7848db93da0ead0a51e
Ryoo Nishida <cu_0918@yahoo.co.jp>
2021-10-01 01:07:38 +0900

    oyasumi

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
    const [count,setCount] = useState(0);
 
 const onClickCountUp = () =>{
+  // stateの値が更新されることで、際レンダリングされる（差分のみ更新）
+  // コンポーネントの値が変わった時に際レンダリングー＞差分を検知して変更を画面に反映する
 setCount(count +1);
 
 }
