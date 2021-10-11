f20c2a0a1e049d0bbc282ef0cd1e02180ab9c54a
Ryoo Nishida <cu_0918@yahoo.co.jp>
2021-10-12 01:29:28 +0900

    statefromrouter

--- src/Page1DetailA.tsx
+++ src/Page1DetailA.tsx
@@ -1,6 +1,10 @@
+import { useLocation } from "react-router-dom"
+
 export const Page1DetailA = () => {
 
+const {state} = useLocation();
+
   return(
-    <h1>Page1DetailAページです</h1>
+    <h1>Page1Detailページです {state}</h1>
   )
 }
